6b23404691a3ea955b267174b4a2e8372394ad3b
ebraikra <ebraikrats@gmail.com>
2024-08-31 20:28:58 +0200

    fix: fixed Clouds last stage

--- Filter/Cloudparticle.gd
+++ Filter/Cloudparticle.gd
@@ -8,48 +8,18 @@ var max_rate: int = 100  # Maximale Rate, wenn CO2 hoch ist
 var alreadyAnalysed: bool = false
 @onready var greyFilter: ColorRect = %GreyFilter
 
+
+##Lernzielrelevant
+##Prüft, ob das Event bereits vorher eingetroffen ist, wenn nicht, wird am Ende des Tages erstmals
+##im Analyse-Bericht davon berichtet, dass Smogwolken auftauchen
+
 #Wird aufgerufen, um dem Filter mitzuteilen wenn sich der CO2 Wert ändert
 #Sorgt dafür, dass je nachdem wie hoch der CO2 Wert ist, dieser mit der "ranges" Variable abgeglichen wird
 #und je nachdem in welcher "Range" sich dieser befindet, die Dichte der Wolken, sowie der "Graufilter" Effekt
 #erhöht wird
-#func update_co2(co2_value: int) -> void:
-	#var ranges: Array = [range(0, 1), range(2, 99), range(100, 199), range(200, 299), range(300, 399), range(400, 499)]
-	#for i: Array in ranges:
-		#if i.has(co2_value):
-			##Lernzielrelevant
-			##Prüft, ob das Event bereits vorher eingetroffen ist, wenn nicht, wird am Ende des Tages erstmals
-			##im Analyse-Bericht davon berichtet, dass Smogwolken auftauchen
-			#if !alreadyAnalysed: 
-				#GlobalSignals.AddAnalysis.emit(GlobalSignals.ANALYSE.CLOUDS_SPAWNING)
-				#alreadyAnalysed = true
-			#match ranges.find(i):
-				#0:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 0.00, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0), 1)
-				#1:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 0.01, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0.00), 1)
-				#2:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 0.05, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0.05), 1)
-				#3:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 0.1, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0.1), 1)
-				#4:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 0.4, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0.4), 1)
-				#5:
-					#var tween: Tween = get_tree().create_tween()
-					#tween.tween_property(self, "amount_ratio", 1, 1)
-					#tween.tween_property(greyFilter, "color", Color(greyFilter.color.r, greyFilter.color.g, greyFilter.color.b, 0.8), 1)
 
 func update_co2(co2_value: int) -> void:
-	var ranges: Array = [range(0, 1), range(2, 99), range(100, 199), range(200, 299), range(300, 499), range(500, 999)]
+	var ranges: Array = [range(0, 1), range(2, 99), range(100, 199), range(200, 299), range(300, 499), range(500, 19999)]
 	var amount_ratios: Array = [0.00, 0.01, 0.05, 0.1, 0.4, 1.0]
 	var alphas: Array = [0, 0.00, 0.05, 0.1, 0.4, 0.8]
 	var index: int = -1
