5696ac950f9034351c1357c6f7397b8b92ced99c
ebraikra <ebraikrats@gmail.com>
2024-10-14 02:07:32 +0200

    feat: update coalplant asset

--- AI/Conditions/IsAtWorkCondition.gd
+++ AI/Conditions/IsAtWorkCondition.gd
@@ -3,8 +3,9 @@ extends ConditionLeaf
 #NPC-Verhaltensabfrage um zu prüfen, ob NPC auf Arbeit ist
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	if actor.isAtWork == true:
-		if actor.workplace.chimney:
+		if actor.workplace is Coalplant:
 			actor.workplace.chimney.show()
+			actor.workplace.chimney2.show()
 		elif actor.workplace is Windpower:
 				actor.workplace.rotor.play("active")
 		elif actor.workplace is Plantfarm:
--- Buildings/Coalplant.gd
+++ Buildings/Coalplant.gd
@@ -2,8 +2,9 @@ class_name Coalplant
 extends Workstation
 
 #Erweiterung der "Lumberjack/Workstation"-Klasse, die lediglich zusätzlich die Schweinchen-Animation abspielt
+@onready var chimney2: AnimatedSprite2D = $Chimney2
 
-
-#func _On_Day_Ended() -> void:
-	##rotor.stop()
-	#SendWorkersHome()
+func _On_Day_Ended() -> void:
+	chimney.hide()
+	chimney2.hide()
+	SendWorkersHome()
